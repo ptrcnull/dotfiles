a6069e082e3baf7bbd6b31c95a3b4692ea845afc
Bjornskjald <github@bjorn.ml>
2019-01-09 00:00:07 +0100

    replaced public ip and time with disk usage

--- .zsh/powerlevel_settings.sh
+++ .zsh/powerlevel_settings.sh
@@ -1,5 +1,5 @@
 # POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator dir vcs)
 # POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs public_ip time)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs disk_usage)
 POWERLEVEL9K_MODE='nerdfont-complete'
