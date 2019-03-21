86359dcb16b60fad1b8639636a11e0ff6c4aa09d
Bjornskjald <github@bjorn.ml>
2019-03-21 20:02:19 +0100

    fixed installing oh-my-zsh

--- install.zsh
+++ install.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 # env, because some OSes keep zsh in /bin (I'm looking at you, Alpine)
 cp .zshrc $HOME
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 cp -r .zsh $HOME
 source ~/.zshrc
