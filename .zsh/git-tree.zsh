f26cb1436d8563b8bc40e0d03332bda90f3a17d4
ptrcnull <git@ptrcnull.me>
2020-12-19 15:37:54 +0100

    fix: More attempts at hiding grep error message

--- .zsh/git-tree.zsh
+++ .zsh/git-tree.zsh
@@ -1,5 +1,5 @@
 if which git > /dev/null; then
-  if ! grep tree ~/.gitconfig 2>&1 >/dev/null; then
+  if ! grep -q tree ~/.gitconfig 2>/dev/null; then
     git config --global alias.tree "log --oneline --graph --decorate --all"
   fi
 fi
