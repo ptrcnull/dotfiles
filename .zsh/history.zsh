5c1accb615a4b47f0bf2ebee51d56a79c3dac38b
ptrcnull <git@ptrcnull.me>
2022-03-28 13:10:44 +0200

    feat: Enlarge history file

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -28,8 +28,8 @@ esac
 
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+[ "$HISTSIZE" -lt 50000000 ] && HISTSIZE=50000000
+[ "$SAVEHIST" -lt 10000000 ] && SAVEHIST=10000000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
