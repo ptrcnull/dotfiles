7d25f334e9873a48f670e272c05cd13144ceb294
ptrcnull <git@ptrcnull.me>
2024-07-07 19:54:02 +0200

    config/mpv/scripts: fix rarbg subs not loading when playing multiple files

--- config/mpv/scripts/rarbg-subs.lua
+++ config/mpv/scripts/rarbg-subs.lua
@@ -1,3 +1,23 @@
-mp.add_hook('on_load', 10, function ()
-    mp.set_property('sub-file-paths', 'Subs:Subs/' .. mp.get_property('filename/no-ext'))
-end)
+local utils = require 'mp.utils'
+
+function set_rarbg_subs_path(name, value)
+       if value ~= nil then
+               local path = mp.get_property('path', '')
+               local dir, fn = utils.split_path(path)
+               local sd = utils.join_path(dir, 'Subs/' .. value)
+
+               local list = utils.readdir(sd, 'files')
+               if not list then
+                       return
+               end
+
+               table.sort(list)
+               for i = 1,#list do
+                       local sub = string.match(list[i], 'eng')
+                       if sub then
+                               mp.commandv('sub-add', utils.join_path(sd, list[i]), 'select')
+                       end
+               end
+       end
+end
+mp.observe_property("filename/no-ext", "string", set_rarbg_subs_path)
