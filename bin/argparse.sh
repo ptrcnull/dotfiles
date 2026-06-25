6a895f61580f885547bdb03e4d0d12c1808611b8
ptrcnull <git@ptrcnull.me>
2026-06-25 13:05:35 +0200

    bin/argparse.sh: fix jank

--- bin/argparse.sh
+++ bin/argparse.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage_header='usage: $(basename "$0") [flags]'
+usage_header='[flags]'
 usage=$'\nflags:'
 
 args=""
@@ -9,47 +9,41 @@ add_arg() {
 	local short="$1"
 	local long="$2"
 	local varname="$3"
-	local help="$4"
-
-	local lusage=""
+	local collect="$4"
+	local help="$5"
 
 	if [ "$short" ]; then
 		echo -n "-$short"
-		lusage="-$short"
 	fi
 
 	if [ "$short" -a "$long" ]; then
 		echo -n "|"
-		lusage="$lusage, "
 	fi
 
 	if [ "$long" ]; then
 		echo -n "--$long"
-		lusage="$lusage--$long"
 	else
 		long="$short"
 	fi
 
 	echo -n ') '
 
-	if [ "$varname" ]; then
+	if [ "$collect" ]; then
 		echo -n "$varname="'"$2"; shift; '
-		lusage="$lusage $varname"
+	elif [ "$varname" ]; then
+		echo -n "$varname='$long'; "
 	else
 		echo -n "$(echo "$long" | tr '-' '_')="'"true"; '
 	fi
 	echo -n 'shift ;;'
-
-	lusage="$lusage: $help"
-
-	usage="$usage"$'\n\t'"$lusage"
 }
 
 get_usage() {
 	local short="$1"
 	local long="$2"
 	local varname="$3"
-	local help="$4"
+	local collect="$4"
+	local help="$5"
 
 	if [ "$short" ]; then
 		echo -n "-$short"
@@ -63,8 +57,8 @@ get_usage() {
 		echo -n "--$long"
 	fi
 
-	if [ "$varname" ]; then
-		echo -n " $varname"
+	if [ "$collect" ]; then
+		echo -n " $collect"
 	fi
 
 	echo -n ": $help"
@@ -81,6 +75,12 @@ for arg; do
 		short="$(echo "$arg" | cut -c2-)"
 		;;
 	=*)
+		collect="$(echo "$arg" | cut -c2-)"
+		if [ ! "$varname" ]; then
+			varname="$collect"
+		fi
+		;;
+	">"*)
 		varname="$(echo "$arg" | cut -c2-)"
 		;;
 	"<"*">")
@@ -89,8 +89,8 @@ for arg; do
 		;;
 	*)
 		if [ "$long" -o "$short" ]; then
-			args="$args"$'\n\t'$(add_arg "$short" "$long" "$varname" "$arg")
-			usage="$usage"$'\n\t'$(get_usage "$short" "$long" "$varname" "$arg")
+			args="$args"$'\n\t'$(add_arg "$short" "$long" "$varname" "$collect" "$arg")
+			usage="$usage"$'\n\t'$(get_usage "$short" "$long" "$varname" "$collect" "$arg")
 			short=""; long=""; varname=""
 		else
 			usage_header="$usage_header $arg"
@@ -102,20 +102,14 @@ done
 usage="$usage_header"$'\n'"$usage"
 
 cat << EOF
-argparse_usage() { echo "$usage"; };
+argparse_usage() { echo "usage: \$(basename "\$1") $usage"; };
 
 for arg; do
 	case "\$arg" in $args
-	-*)
-		argparse_usage
-		if [ "\$arg" = "-h" -o "\$arg" = "--help" ]; then
-			exit 0
-		else
-			exit 1
-		fi
-		;;
+	-h|--help) argparse_usage "\$0"; exit 0 ;;
+	-*) argparse_usage "\$0"; exit 1 ;;
 	*) set -- "\$@" "\$arg"; shift ;;
 	esac
 done
-if [ "\$#" -lt "$required" ]; then argparse_usage; exit 1; fi
+if [ "\$#" -lt "$required" ]; then argparse_usage "\$0"; exit 1; fi
 EOF
